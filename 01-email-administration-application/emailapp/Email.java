29968aeda0b24dee675a3395e751fc60090eacf4
minpyaythein <paukking@gmail.com>
2023-09-18 22:02:16 +0900

    adding email administration app (constructor, department, random password)

---
+++ 01-email-administration-application/emailapp/Email.java
@@ -0,0 +1,61 @@
+package emailapp;
+
+import java.util.Scanner;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private int defaultPasswordLength = 10;
+    private String alternateEmail;
+
+    // Constructor to receive the first name and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+
+        // Call a method asking for the department - return the department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+
+        // Call a method asking for the department - return the department
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Password: " + this.password);
+    }
+
+    // Ask for the department
+    private String setDepartment() {
+        System.out.print(
+                "Enter department code\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+        Scanner input = new Scanner(System.in);
+        int depChoice = input.nextInt();
+        if (depChoice == 1) {
+            return "sales";
+        } else if (depChoice == 2) {
+            return "dev";
+        } else if (depChoice == 3) {
+            return "accounting";
+        }
+        return "";
+    }
+
+    // Generate a random password
+    private String randomPassword(int length) {
+        String passwordSetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!#$%&@*{[+-=~^]}";
+        char[] password = new char[length];
+        for (int i = 0; i < length; i++) {
+            int rand = (int) (Math.random() * passwordSetter.length());
+            password[i] = passwordSetter.charAt(rand);
+        }
+        return new String(password);
+    }
+
+    // Set the mailbox capacity
+
+    // Set the alternate email
+
+    // Change the password
+}
---
+++ 01-email-administration-application/emailapp/EmailApp.java
@@ -0,0 +1,8 @@
+package emailapp;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+        Email email1 = new Email("MIN", "PYAY THEIN");
+    }
+}
